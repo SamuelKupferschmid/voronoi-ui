e114e7454e171bece284602fba463c790597f92c
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-11-28 16:05:53 +0100

    setup solution

---
+++ Main.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace VoronoiUI
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.canvas = new System.Windows.Forms.PictureBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.Window;
+            this.canvas.Location = new System.Drawing.Point(0, 0);
+            this.canvas.Margin = new System.Windows.Forms.Padding(0);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(633, 513);
+            this.canvas.TabIndex = 0;
+            this.canvas.TabStop = false;
+            this.canvas.Click += new System.EventHandler(this.canvas_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(635, 11);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(73, 29);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear Points";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 513);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.canvas);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Voronoi by Samuel Kupferschmid";
+            ((System.ComponentModel.ISupportInitialize)(this.canvas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox canvas;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VoronoiUI
+{
+    public partial class Main : Form
+    {
+        private List<Point> _points = new List<Point>();
+        private Bitmap _image;
+        private Voronoi _voronoi;
+
+        public Main()
+        {
+            InitializeComponent();
+            _voronoi = new Voronoi(Distance.Euclidean);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            _image = new Bitmap(canvas.Width,canvas.Height);
+            canvas.Image = _image;
+        }
+
+        private void canvas_Click(object sender, EventArgs e)
+        {
+            var ev = e as MouseEventArgs;
+            _points.Add(new Point(ev.X,ev.Y));
+            DrawCanvas();
+
+        }
+
+        private void DrawCanvas()
+        {
+            var edges = _voronoi.CalcEdges(_points);
+            using (var g = Graphics.FromImage(_image))
+            {
+                g.Clear(BackColor);
+                foreach (var p in _points)
+                {
+                    g.FillEllipse(Brushes.CornflowerBlue,(float)p.X,(float)p.Y, 5, 5);
+                }
+
+                foreach (var e in edges)
+                {
+                    g.DrawLine(Pens.Blue, (float)e.Start.X,(float)e.Start.Y,(float)e.End.X,(float)e.End.Y);
+                }
+            }
+            canvas.Invalidate();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            _points.Clear();
+            DrawCanvas();
+        }
+    }
+}
