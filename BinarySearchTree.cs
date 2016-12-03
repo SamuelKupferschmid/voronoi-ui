e41b486ead7ffec34315b075e68242be48f7012b
Samuel Kupferschmid <samuel.kupferschmid@gmail.com>
2016-12-03 11:51:35 +0100

    added Beachline and BinarySearchTree classes

---
+++ BinarySearchTree.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoronoiUI
+{
+    public class BinarySearchTree<T>
+    {
+        private readonly Comparison<T> _comparison;
+
+        public BinarySearchTree(Comparison<T> comparison)
+        {
+            _comparison = comparison;
+        }
+
+        protected Node<T> Root;
+
+        public void Insert(T value)
+        {
+            
+        }
+
+        public void Delete(T value)
+        {
+            
+        }
+
+        public bool Contains(T value)
+        {
+            return false;
+        }
+
+        public class Node<T>
+        {
+            public readonly T Value;
+            public Node<T> Left { get; set; } 
+            public Node<T> Right { get; set; }
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
